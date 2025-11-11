e0328348c8e8b139f7dffcccdf8372d0fdd423d1
Skoivumaki <sakari.koivumaki@gmail.com>
2025-11-11 10:45:47 +0200

    initial version of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import SpotifyPlayer from "./components/SpotifyPlayer";
+import Link from "next/link";
+import { Button, ButtonTheme, ButtonSize } from "./components/Button";
+import { Container } from "./components/Container";
 import UserProfile from "./components/UserProfile";
 import { cookies } from "next/headers";
-import Playlists from "./components/Playlists";
-import Link from "next/dist/client/link";
 
 export default async function PlayerPage() {
   const cookieStore = await cookies();
@@ -10,10 +10,59 @@ export default async function PlayerPage() {
 
   return (
     <>
-      <SpotifyPlayer access_token={accessToken} />
-      <UserProfile />
-      <Playlists />
-      <Link href="/play">Play</Link>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: "#121212",
+          minHeight: "100vh",
+          padding: "1rem",
+          gap: "1rem",
+        }}
+      >
+        <h1 className="text-center font-bold text-lg">Quiz App prototype</h1>
+        <Container
+          direction="row"
+          justify="between"
+          align="center"
+          gap="4"
+          className="w-full bg-gray-800 p-4 rounded"
+        >
+          SHOWCASE INFO
+        </Container>
+        <Container
+          direction="col"
+          justify="between"
+          align="center"
+          gap="4"
+          className="w-full bg-gray-800 p-4 rounded"
+        >
+          <h1>{"Try for free (testers proceed here)"}</h1>
+          <Button theme={ButtonTheme.PRIMARY} size={ButtonSize.XL}>
+            <Link href="/play">Play</Link>
+          </Button>
+        </Container>
+        <Container
+          direction="row"
+          justify="between"
+          align="center"
+          gap="4"
+          className="w-full bg-gray-800 p-4 rounded"
+        >
+          <h1>{"BILLING INFO"}</h1>
+        </Container>
+        <Container
+          direction="col"
+          justify="between"
+          align="center"
+          gap="4"
+          className="w-full bg-gray-800 p-4 rounded"
+        >
+          <h1>{"PROFILE INFO"}</h1>
+          <UserProfile />
+        </Container>
+      </div>
     </>
   );
 }
