7d4440d17f8f649936a1a05e1d64c142a4ec31ba
Sakari Koivumäki <123396118+Skoivumaki@users.noreply.github.com>
2025-11-12 23:36:57 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+basePath: '/quiz',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -19,9 +20,5 @@ const nextConfig: NextConfig = {
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-  basePath: '/quiz',
-}
 
 export default nextConfig;
