86f3a6c10ebdc1e3a1f85f3a36506cd3c53880eb
Skoivumaki <sakari.koivumaki@gmail.com>
2025-11-13 20:49:13 +0200

    fix: margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,14 @@ export default async function PlayerPage() {
           minHeight: "100vh",
         }}
       >
-        <div className="flex flex-row sticky w-full top-0 bg-gray-950 text-center justify-between items-center h-10">
+        <div className="flex flex-row fixed w-full top-0 bg-gray-950 text-center justify-between items-center h-10">
           <div className="p-2 text-center justify-between items-center flex flex-row fixed w-full bg-gradient-to-t from-indigo-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">
             <span>menu</span>
             <h1 className="font-bold text-lg">Quiz App</h1>
             <span>profile</span>
           </div>
         </div>
-        <div className="bg-gradient-to-t from-gray-950 via-gray-700 to-gray-950 flex flex-col p-4 h-68 justify-center">
+        <div className="bg-gradient-to-t from-gray-950 via-gray-700 to-gray-950 flex flex-col p-4 h-68 justify-center mt-5">
           <h1 className="w-55 text-left font-bold text-lg bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">
             Make your music more fun with Quiz App
           </h1>
