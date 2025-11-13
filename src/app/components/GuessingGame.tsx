6f27dc01393b742d18a2a2fe11b73deb2ada07c8
Skoivumaki <sakari.koivumaki@gmail.com>
2025-11-13 19:08:27 +0200

    cleanup

--- src/app/components/GuessingGame.tsx
+++ src/app/components/GuessingGame.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 import { usePlayTrackMutation } from "@/quizApi";
+import { Button, ButtonSize, ButtonTheme } from "./Button";
+import Link from "next/link";
 
 interface FormattedTrack {
   id: string;
@@ -189,14 +191,37 @@ export default function GuessingGame({
           disabled={isShowAnswerDisabled}
           className={`
     border border-neutral-600 rounded-md px-4 py-2 font-semibold
-    transition-all duration-300
+    transition-all duration-300 flex flex-row items-center justify-center
     ${
       isShowAnswerDisabled
-        ? "bg-neutral-700 text-gray-400 cursor-not-allowed opacity-70 animate-pulse"
+        ? "bg-neutral-700 text-gray-400 cursor-not-allowed opacity-100"
         : "bg-neutral-800 hover:bg-neutral-700 text-white cursor-pointer"
     }
   `}
         >
+          {isShowAnswerDisabled && (
+            <svg
+              class="mr-3 -ml-1 size-5 animate-spin text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          )}
+
           {isShowAnswerDisabled ? "Show Answer" : "Show Answer"}
         </button>
       ) : isFinished ? null : (
