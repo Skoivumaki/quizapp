ebaca80785ab1aaae112b35cec89cb5afcf15694
Skoivumaki <sakari.koivumaki@gmail.com>
2025-12-31 17:17:28 +0200

    more safetys to refresh token if needed

--- src/app/play/layout.tsx
+++ src/app/play/layout.tsx
@@ -3,7 +3,7 @@ import { cookies } from "next/dist/server/request/cookies";
 import LoginPrompt from "../components/LoginPrompt";
 
 export const metadata: Metadata = {
-  title: "Quiz App",
+  title: "Guess the Track - Play",
 };
 
 export default async function RootLayout({
@@ -14,9 +14,19 @@ export default async function RootLayout({
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("spotify_access_token")?.value;
 
+  const expiresAtString = cookieStore.get("spotify_token_expires_at")?.value;
+
+  const now = Date.now();
+  const expiresAt = expiresAtString ? Number(expiresAtString) : 0;
+  const isExpired = now > expiresAt;
+
+  if (isExpired && accessToken) {
+    console.log("REFRESH: Expired token and has accessToken");
+  }
   return (
     <>
       {!accessToken && <LoginPrompt />}
+      {isExpired && <LoginPrompt />}
       {children}
     </>
   );
